677e8859d46ace36044b1bd980648f236631e5c2
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2015-10-22 14:17:44 +0200

    Webview Update: Stable 45.0.2454.95 - Beta 46.0.2490.64
    Use stable version by default:
    
    By default for all stable version is used.
    To use beta version add into your device boardconfig:
    
    PREBUILT_WEBVIEW_VERSION := beta
    
    Credit: José Adolfo Galdámez
    
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- chromium/Android.mk
+++ chromium/Android.mk
@@ -13,89 +13,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 # This package provides the 'glue' layer between Chromium and WebView.
+#
 
 LOCAL_PATH := $(call my-dir)
-CHROMIUM_PATH := external/chromium_org
+
+ifndef PREBUILT_WEBVIEW_VERSION
+    PREBUILT_WEBVIEW_VERSION := stable
+endif
+
 
 # Prebuilt com.google.android.webview apk
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := webview
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE        := webview
+LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS  := APPS
+LOCAL_CERTIFICATE   := PRESIGNED
+LOCAL_SRC_FILES     := prebuilt/$(PREBUILT_WEBVIEW_VERSION)/$(TARGET_ARCH)/webview.apk
 
-ifeq ($(TARGET_ARCH),arm64)
-        LOCAL_SRC_FILES := prebuilt/arm64/webview.apk
+ifeq ($(TARGET_IS_64_BIT),true)
+     # Primary arch
+     $(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
+     $(shell cp $(LOCAL_PATH)/prebuilt/$(PREBUILT_WEBVIEW_VERSION)/$(TARGET_ARCH)/$(TARGET_CPU_ABI)/libwebviewchromium.so $(TARGET_OUT_SHARED_LIBRARIES))
 
-        # Primary arch
-        $(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
-        $(shell cp $(LOCAL_PATH)/prebuilt/arm64/lib/arm64-v8a/libwebviewchromium.so $(TARGET_OUT_SHARED_LIBRARIES))
+     $(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/$(TARGET_ARCH))
+     $(shell ln -sf ../../../../lib64/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/$(TARGET_ARCH)/libwebviewchromium.so)
+     ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/$(TARGET_ARCH)/libwebviewchromium.so
 
-        $(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/arm64)
-        $(shell ln -sf ../../../../lib64/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/arm64/libwebviewchromium.so)
-        ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/arm64/libwebviewchromium.so
+     # Secondary arch
+     $(shell mkdir -p $($(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_SHARED_LIBRARIES))
+     $(shell cp $(LOCAL_PATH)/prebuilt/$(PREBUILT_WEBVIEW_VERSION)/$(TARGET_ARCH)/$(TARGET_2ND_CPU_ABI)/libwebviewchromium.so $($(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_SHARED_LIBRARIES))
 
-        # Secondary arch
-        $(shell mkdir -p $($(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_SHARED_LIBRARIES))
-        $(shell cp $(LOCAL_PATH)/prebuilt/arm64/lib/armeabi-v7a/libwebviewchromium.so $($(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_SHARED_LIBRARIES))
-
-        $(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/arm)
-        $(shell ln -sf ../../../../lib/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/arm/libwebviewchromium.so)
-        ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/arm/libwebviewchromium.so
-else ifeq ($(TARGET_ARCH),arm)
-        LOCAL_SRC_FILES := prebuilt/arm/webview.apk
-
-        $(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
-        $(shell cp $(LOCAL_PATH)/prebuilt/arm/lib/armeabi-v7a/libwebviewchromium.so $(TARGET_OUT_SHARED_LIBRARIES))
-
-        $(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/arm)
-        $(shell ln -sf ../../../../lib/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/arm/libwebviewchromium.so)
-        ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/arm/libwebviewchromium.so
-else ifeq ($(TARGET_ARCH),x86)
-        LOCAL_SRC_FILES := prebuilt/x86/webview.apk
-
-        $(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
-        $(shell cp $(LOCAL_PATH)/prebuilt/x86/lib/x86/libwebviewchromium.so $(TARGET_OUT_SHARED_LIBRARIES))
-
-        $(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/x86)
-        $(shell ln -sf ../../../../lib/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/x86/libwebviewchromium.so)
-        ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/x86/libwebviewchromium.so
+     $(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/$(TARGET_2ND_ARCH))
+     $(shell ln -sf ../../../../lib/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/$(TARGET_2ND_ARCH)/libwebviewchromium.so)
+     ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/$(TARGET_2ND_ARCH)/libwebviewchromium.so
 else
-        # Huh? You MIPS, bro?
-        $(error Prebuilt WebView: unsupported architecture - $(TARGET_ARCH))
+     $(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
+     $(shell cp $(LOCAL_PATH)/prebuilt/$(PREBUILT_WEBVIEW_VERSION)/$(TARGET_ARCH)/libwebviewchromium.so $(TARGET_OUT_SHARED_LIBRARIES))
+
+     $(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/$(TARGET_ARCH))
+     $(shell ln -sf ../../../../lib/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/$(TARGET_ARCH)/libwebviewchromium.so)
+     ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/$(TARGET_ARCH)/libwebviewchromium.so
 endif
 
 include $(BUILD_PREBUILT)
+
 
 # Native support library (libwebviewchromium_plat_support.so) - does NOT link
 # any native chromium code.
 include $(CLEAR_VARS)
 
-LOCAL_MODULE:= libwebviewchromium_plat_support
+LOCAL_MODULE := libwebviewchromium_plat_support
 
-LOCAL_SRC_FILES:= \
-        plat_support/draw_gl_functor.cpp \
-        plat_support/jni_entry_point.cpp \
-        plat_support/graphics_utils.cpp \
-        plat_support/graphic_buffer_impl.cpp \
+LOCAL_SRC_FILES := \
+    plat_support/draw_gl_functor.cpp \
+    plat_support/jni_entry_point.cpp \
+    plat_support/graphics_utils.cpp \
+    plat_support/graphic_buffer_impl.cpp
 
-LOCAL_C_INCLUDES:= \
-        $(CHROMIUM_PATH) \
-        external/skia/include/core \
-        frameworks/base/core/jni/android/graphics \
-        frameworks/native/include/ui \
+LOCAL_C_INCLUDES := \
+    external/chromium_org \
+    external/skia/include/core \
+    frameworks/base/core/jni/android/graphics \
+    frameworks/native/include/ui
 
 LOCAL_SHARED_LIBRARIES += \
-        libandroid_runtime \
-        liblog \
-        libcutils \
-        libskia \
-        libui \
-        libutils \
+    libandroid_runtime \
+    liblog \
+    libcutils \
+    libskia \
+    libui \
+    libutils
 
 LOCAL_MODULE_TAGS := optional
 
@@ -109,21 +99,14 @@ include $(BUILD_SHARED_LIBRARY)
 # Does NOT link any native chromium code.
 include $(CLEAR_VARS)
 
-LOCAL_MODULE:= libwebviewchromium_loader
-
-LOCAL_SRC_FILES := \
-        loader/loader.cpp \
-
-LOCAL_CFLAGS := \
-        -Werror \
-
-LOCAL_SHARED_LIBRARIES += \
-        libdl \
-        liblog \
-
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE           := libwebviewchromium_loader
+LOCAL_SRC_FILES        := loader/loader.cpp
+LOCAL_CFLAGS           := -Werror
+LOCAL_SHARED_LIBRARIES += libdl liblog
+LOCAL_MODULE_TAGS      := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
 
 # Build other stuff
 include $(call first-makefiles-under,$(LOCAL_PATH))
